6cf8d4dc334c286a843a645207f4603d14acbf56
Clairova <echo.rangrang@gmail.com>
2025-09-04 11:46:15 +0700

    Clairova: added test comment

--- src/main/java/net/rang/clairova/Clairova.java
+++ src/main/java/net/rang/clairova/Clairova.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// test comment
 public class Clairova implements ModInitializer {
 	public static final String MOD_ID = "clairova";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
