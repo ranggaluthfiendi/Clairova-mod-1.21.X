559c44de2ec4abfaf192701a2795a783dccba36e
Clairova <echo.rangrang@gmail.com>
2025-09-04 11:44:01 +0700

    Initial commit

---
+++ src/main/java/net/rang/clairova/Clairova.java
@@ -0,0 +1,16 @@
+package net.rang.clairova;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Clairova implements ModInitializer {
+	public static final String MOD_ID = "clairova";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/rang/clairova/ClairovaClient.java
@@ -0,0 +1,10 @@
+package net.rang.clairova;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClairovaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
